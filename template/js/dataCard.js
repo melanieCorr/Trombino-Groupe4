d9ebef57941c1ec663b75bf0be51ae4f8f9ab0c2
melanieCorr <mel.correia.92@gmail.com>
2020-11-30 13:11:15 +0100

    Ajout de 2 filtres

--- template/js/dataCard.js
+++ template/js/dataCard.js
@@ -4,19 +4,33 @@
 }); */
 
 let url = "https://jardindesconnaissances.univ-paris8.fr/THYP/20-21/g4/omk"; 
+let dataYoutubeurs; 
+let dataChaines; 
 
-d3.csv("../data/api.csv", function(data) {
-    data.forEach(function(d) {
-        if (d.titre === 'Liste des youtubeurs') {
-            d3.json(url + d.url, function(res) {
-                displayItems(res);
-            });
-        }
-    }) 
-})
+d3.queue().defer(d3.csv, '../data/api.csv')
+    .awaitAll(function(error, results) {
+        if (error) throw error;
+        results.forEach(function(res) {
+            res.forEach(api => {
+                if (api.titre === 'Liste des youtubeurs') {
+                    console.log(api)
+                    d3.json(url + api.url, function(res) {
+                        console.log(res)
+                        displayItems(res); 
+                        dataYoutubeurs = res; 
+                    });
+                }
+                else if (api.titre === 'Liste des chaines') {
+                    d3.json(url + api.url, function(res) {
+                        dataChaines = res; 
+                    })
+                }
+            }) 
+        }) 
+    }); 
 
 function displayItems(data) {
-
+    
     var card = d3.select('#youtube-cards-items').selectAll('div').data(data).enter().append('div')
         .attr('class', 'justify-content-center d-inline-block')
         .style('margin', '30px').append('div')
@@ -37,4 +51,57 @@ function displayItems(data) {
                 'src="' + d["thumbnail_display_urls"]["square"] + '" alt="' + d["chaine:voc.ttlhasChannel"][0]["@value"] + '" />'; 
             return img
         })
+}
+
+function genreFilter(genre) {
+    let cards = document.getElementById('youtube-cards-items'); 
+    cards.innerHTML = ''; 
+    if (genre === 'Tous') {
+        setTimeout(() => {
+            displayItems(dataYoutubeurs); 
+        }, 1000);
+    } 
+    else {
+        setTimeout(() => {
+            let youtubeurs = [];
+            dataChaines.forEach(chaine => {
+                if (chaine['dcterms:type'][0]['@value'].includes(genre)) {
+                    dataYoutubeurs.forEach(d => {  
+                        if (chaine['chaine:voc.ttlnomChaine'][0]['@value'] === d['chaine:voc.ttlhasChannel'][0]['@value']) {
+                             youtubeurs.push(d); 
+                        }
+                    })
+                }
+            })
+            displayItems(youtubeurs)
+        }, 1000); 
+    }
+}
+
+function dateFilter(date1, date2) {
+    let cards = document.getElementById('youtube-cards-items'); 
+    cards.innerHTML = ''; 
+    if (date1 === 'Tous') {
+        setTimeout(() => {
+            displayItems(dataYoutubeurs); 
+        }, 1000);
+    } 
+    else {
+        setTimeout(() => {
+            let youtubeurs = [];
+            dataChaines.forEach(chaine => {
+                if (
+                    chaine['dcterms:created'][0]['@value'].substring(0, 4) >= date1 && 
+                    chaine['dcterms:created'][0]['@value'].substring(0, 4) <= date2
+                ) {
+                    dataYoutubeurs.forEach(d => {  
+                        if (chaine['chaine:voc.ttlnomChaine'][0]['@value'] === d['chaine:voc.ttlhasChannel'][0]['@value']) {
+                             youtubeurs.push(d); 
+                        }
+                    })
+                }
+            })
+            displayItems(youtubeurs)
+        }, 1000); 
+    }
 }
--- template/js/infoPage.js
+++ template/js/infoPage.js
@@ -5,32 +5,44 @@ var paramPrenom = window.location.search.substring(5);
 }); */
 
 let url = "https://jardindesconnaissances.univ-paris8.fr/THYP/20-21/g4/omk"; 
+let youtubeChaine; 
 
 d3.csv("../data/api.csv", function(data) {
     data.forEach(function(d) {
-        if (d.titre.includes(paramPrenom)) {
-            console.log(d); 
-            d3.json(url + d.url, function(res) {
-                infoData(res);
+        if (d.titre.includes(decodeURI(paramPrenom))) {
+            d3.json(url + d.url, function(resYoutubeur) {
+                data.forEach(function(d) {
+                    if (d.titre.includes(resYoutubeur['chaine:voc.ttlhasChannel'][0]['@value'])) {
+                        d3.json(url + d.url, function(resChaine) {
+                            infoData(resYoutubeur, resChaine); 
+                        })
+                    }
+                })
             });
         }
     }) 
 })
 
-function infoData(data) {
+function infoData(dataYoutubeur, dataChaine) {
     var media = d3.select('#media')
         media.append('img').attr('class', 'align-self-center mr-3').style('height', '15rem')
         .style('max-height', '100%').style('max-width', '100%').attr('src', function() {
-            return data["thumbnail_display_urls"]["square"];
+            return dataYoutubeur["thumbnail_display_urls"]["square"];
         })
   
     var mediaBody = media.append('div').attr('class', 'media-body').attr('id', 'media-body')
-        mediaBody.append('h5').text(data["foaf:family_name"][0]["@value"] + " " + data["foaf:firstName"][0]["@value"])
+        mediaBody.append('h5').text(dataYoutubeur["foaf:family_name"][0]["@value"] + " " + dataYoutubeur["foaf:firstName"][0]["@value"])
         mediaBody.append('br')
-        mediaBody.append('span').attr('class', 'fa fa-birthday-cake').text(" " + data["foaf:birthday"][0]["@value"])
+        mediaBody.append('span').attr('class', 'fa fa-birthday-cake').text(" " + dataYoutubeur["foaf:birthday"][0]["@value"])
         mediaBody.append('br')
-        mediaBody.append('span').attr('class', 'fa fa-thumbs-up').text(" " + data["foaf:interest"][0]["@value"])
+        mediaBody.append('span').attr('class', 'fa fa-thumbs-up').text(() => {
+            if (dataYoutubeur["foaf:interest"]) {
+                return " " + dataYoutubeur["foaf:interest"][0]["@value"]
+            } else {
+                return " "
+            }
+        }) 
         mediaBody.append('br')
-        mediaBody.append('a').style('color', 'red').attr('href', data["chaine:voc.ttlhasChannel"][1]["@id"])
-                .append('span').attr('class', 'fa fa-youtube-play').text(" " + data["chaine:voc.ttlhasChannel"][0]["@value"])
+        mediaBody.append('a').style('color', 'red').attr('href', dataYoutubeur["chaine:voc.ttlhasChannel"][1]["@id"])
+                .append('span').attr('class', 'fa fa-youtube-play').text(" " + dataYoutubeur["chaine:voc.ttlhasChannel"][0]["@value"])
 }
